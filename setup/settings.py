cb28737a31339d33bb6d71d300261fbde2ba41e6
pedro cangemi <pedrocangemi@gmail.com>
2024-03-06 16:59:27 -0300

    first commit

--- setup/settings.py
+++ setup/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_mhawr2c5)0=$bf)w2rx#mix$nwl36p#)w&b9ciwe0#y+7sdoq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
